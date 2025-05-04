77c96d29f78e5d81df5a113363dba2fa911c0717
DominicDev <s0l0estudi4r@gmail.com>
2025-05-04 16:06:05 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Foto Preview](preview/20-projects-using-cascade-css.avif)](https://20essentials.github.io/20-projects-using-cascade-css)
+
---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: "static" ,
+  },
+  base: '/20-projects-using-cascade-css/',
+});
